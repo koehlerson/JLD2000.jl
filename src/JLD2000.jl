287bbff570d314115995c5384106ad6f4e4bc876
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2023-12-01 10:26:02 +0100

    init

--- src/JLD2000.jl
+++ src/JLD2000.jl
@@ -1,5 +1,20 @@
 module JLD2000
 
-# Write your package code here.
+struct MockFile
+    d::Dict{String,Any}
+end
+
+function MockFile()
+    return MockFile(Dict())
+end
+
+function Base.setindex!(file::MockFile, v0, key)
+    file.d[key] = optional_convert(v0)
+end
+
+optional_convert(v0) = v0
+
+export MockFile
+export optional_convert
 
 end
